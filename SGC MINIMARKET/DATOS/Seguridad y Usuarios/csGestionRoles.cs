e6f20d7a2981b1c8e78a0237117588df051adec0
Darwin Sánchez Vera <dsanchezv@uteq.edu.ec>
2025-08-19 20:16:51 -0500

    Finalizacion de la tarea 1 (crear clase para la tabla rol)

---
+++ SGC MINIMARKET/DATOS/Seguridad y Usuarios/csGestionRoles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATOS.Seguridad_y_Usuarios
+{
+    internal class csGestionRoles
+    {
+        public int InsertarRoles(string nombre)
+        {
+            int resultado = 0;
+            return resultado;
+        }
+
+        public int ActualizarRoles(string nombre)
+        {
+            int resultado = 0;
+            return resultado;
+        }
+
+        public int EliminarRoles(string nombre)
+        {
+            int resultado = 0;
+            return resultado;
+        }
+
+        public DataTable ConsultarRoles(string nombre)
+        {
+            DataTable resultado = new DataTable();
+            return resultado;
+        }
+    }
+}
