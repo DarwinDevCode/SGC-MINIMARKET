38e0117f19ffb8e9077d55d199a3e61315d80c15
AnthonyArana1725 <aaranam5@uteq.edu.ec>
2025-08-19 23:52:20 -0500

    Cambios para la Main

---
+++ SGC MINIMARKET/SGC MINIMARKET/Proveedores y Compras/frmExample4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SGC_MINIMARKET.Proveedores_y_Compras
+{
+    public partial class frmExample4: Form
+    {
+        public frmExample4()
+        {
+            InitializeComponent();
+        }
+    }
+}
